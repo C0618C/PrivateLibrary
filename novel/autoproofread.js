6bf3926f426cce1bdc0a085bf1ea05760a89dc2c
C0618C <weicong75@gmail.com>
2020-08-25 22:21:29 +0800

    修正没有设定规则时报错的问题

--- novel/autoproofread.js
+++ novel/autoproofread.js
@@ -3,6 +3,10 @@ const Cache = require("./cache").Cache;
 
 function Proofread(ruleid, content) {
     let ruleSetting = Cache.GetProofread();
+    if (!Array.isArray(ruleSetting.common) && !Array.isArray(ruleid)) return content;
+    if (!Array.isArray(ruleSetting.common)) ruleSetting.common = [];
+    if (!Array.isArray(ruleid)) ruleid = [];
+    
     let allrule = Array.of(...ruleSetting.common, ...ruleid);
     for (let id of allrule) {
         let rule = ruleSetting[id];
