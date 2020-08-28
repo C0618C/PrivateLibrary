43884396be0632ad4b11c3633ff6ae47a190aecd
C0618C <weicong75@gmail.com>
2020-08-29 02:18:21 +0800

    内容重复警告功能

--- novel/autoproofread.js
+++ novel/autoproofread.js
@@ -1,12 +1,17 @@
+const fs = require('fs');             //
 const Cache = require("./cache").Cache;
 
-
+/**
+ * 根据规则对内容进行修正（查找替换）
+ * @param {*} ruleid 
+ * @param {*} content 
+ */
 function Proofread(ruleid, content) {
     let ruleSetting = Cache.GetProofread();
     if (!Array.isArray(ruleSetting.common) && !Array.isArray(ruleid)) return content;
     if (!Array.isArray(ruleSetting.common)) ruleSetting.common = [];
     if (!Array.isArray(ruleid)) ruleid = [];
-    
+
     let allrule = Array.of(...ruleSetting.common, ...ruleid);
     for (let id of allrule) {
         let rule = ruleSetting[id];
@@ -20,5 +25,40 @@ function Proofread(ruleid, content) {
 }
 
 
+/**
+ * 抽检所给章节是否有雷同的
+ * @param {Array} items 包含filepath属性的对象数组，检查结果按原样返回。
+ */
+function CheckFilesSimilarity(items) {  //传入章节地址，校验是否存在重复章节
+    let result = [];
+    for (let i = 0; i < items.length; i++) {
+        let curText = fs.readFileSync(items[i].filepath).toString();
+        if (curText.length < 50) {
+            console.log("文件过短，可能存在问题：", item[i], curText);
+            continue;
+        }
+        let p1 = Math.floor(curText.length / 4);
+        let p2 = Math.floor(curText.length / 3);
+        let p3 = Math.abs(curText.length - p1);
+        let idxText = [curText.substr(p1, 30), curText.substr(p2, 30), curText.substr(p3, 30)];
+
+        for (let j = i + 1; j < items.length; j++) {
+            let ckText = fs.readFileSync(items[j].filepath).toString();
+
+            let c = 0;
+            if (ckText.includes(idxText[0])) c++;
+            if (ckText.includes(idxText[1])) c++;
+            if (ckText.includes(idxText[2])) c++;
+            if (c >= 2) {                   //可能重复的只有其中一半 不保证三段完全都找到
+                result.push({ a: items[i], b: items[j] });
+            }
+        }
+    }
+
+    return result;
+}
+
+
 
 exports.Proofread = Proofread;
+exports.CheckFilesSimilarity = CheckFilesSimilarity;
--- novel/loader.js
+++ novel/loader.js
@@ -12,6 +12,7 @@ const PDFCreater = require("./pdf");
 const { GetTextByURL, GetTempPathByUrl } = require("../dlfromurl");
 const { ChangeRule, GetRule, GetHost } = require("./loader_rule");
 const { QS_GetValueBySetting } = require("./analyzer")
+const { CheckFilesSimilarity } = require("./autoproofread");
 
 let Solution;
 let Servers;
@@ -234,7 +235,13 @@ function __R2_CatchUrlFinishCallback(novel, jobSetting, checkChapters, retryTime
     let dir = Cache.GetNovelCachePath(novel.title) + "/";
     jobSetting.chapters.forEach(file => {
         file.filepath = dir + file.file;
-    })
+    });
+
+    let result = CheckFilesSimilarity(jobSetting.chapters);
+    if (Array.isArray(result) && result.length > 0) {
+        console.warn("这些章节可能存在重复内容：", result);
+        return;     //TODO: 临时处理，应该发到前端通知
+    }
 
     if (jobSetting.iscompress) {//选择了合并
         console.log("开始合并TXT文件！！")
